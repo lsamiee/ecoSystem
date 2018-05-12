5b44925ed25b27d0b16c044270ea2780da085306
Leyla Samiee <lsamie@gmail.com>
2018-05-12 14:33:10 -0400

    Update Sheep.java

--- src/Sheep.java
+++ src/Sheep.java
@@ -5,9 +5,4 @@ import javax.imageio.*;
 
 public class Sheep extends JPanel {
 
-    public void paint(Graphics g) {
-        Image sheep = Toolkit.getDefaultToolkit().getImage("sheep.png");
-        g.drawImage(sheep, 10,10, 100, 100,this);
-    }
-
 }

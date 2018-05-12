5ff33b3a6587a9f7b65c04a145c81206c62b4910
Leyla Samiee <lsamie@gmail.com>
2018-05-12 14:33:29 -0400

    Update Wolf.java

--- src/Wolf.java
+++ src/Wolf.java
@@ -4,9 +4,5 @@ import javax.swing.JPanel;
 
 public class Wolf extends JPanel {
 
-    public void paint(Graphics g) {
 
-        Image wolf = Toolkit.getDefaultToolkit().getImage("wolf.png");
-        g.drawImage(wolf, 110,110, 100, 100,this);
-    }
 }

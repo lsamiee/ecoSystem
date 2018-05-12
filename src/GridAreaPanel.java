7de609ee58fb92c56f993216391ae78ce817bb13
Leyla Samiee <lsamie@gmail.com>
2018-05-12 14:31:28 -0400

    Update GridAreaPanel.java

--- src/GridAreaPanel.java
+++ src/GridAreaPanel.java
@@ -4,16 +4,31 @@ import javax.swing.JComponent;
 import javax.swing.JPanel;
 import javax.imageio.*;
 
-class GridAreaPanel extends JPanel
-{
+class GridAreaPanel extends JPanel {
     Sheep sheep = new Sheep();
     Wolf wolf = new Wolf();
     Plant plant = new Plant();
 
     public void paint(Graphics g) {
-        this.sheep.paint(g);
-        this.wolf.paint(g);
-        this.plant.paint(g);
-        this.setVisible(true);
+        paintSheep(g);
+        paintWolf(g);
+        paintPlant(g);
+        setVisible(true);
+    }
+
+    private void paintSheep(Graphics g) {
+        Image sheep = Toolkit.getDefaultToolkit().getImage("sheep.png");
+        g.drawImage(sheep, 10, 10, 100, 100, this);
+    }
+
+    private void paintWolf(Graphics g) {
+
+        Image wolf = Toolkit.getDefaultToolkit().getImage("wolf.png");
+        g.drawImage(wolf, 110, 110, 100, 100, this);
+    }
+
+    private void paintPlant(Graphics g) {
+        Image plant = Toolkit.getDefaultToolkit().getImage("plant.png");
+        g.drawImage(plant, 210, 210, 100, 100, this);
     }
 }

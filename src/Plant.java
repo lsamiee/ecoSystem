74fbf6ed286f47ea7d9852cdc684b75f5b0efed7
Leyla Samiee <lsamie@gmail.com>
2018-05-12 14:32:47 -0400

    Update Plant.java

--- src/Plant.java
+++ src/Plant.java
@@ -1,16 +1,7 @@
 import java.awt.*;
 import javax.swing.JPanel;
 
-
-public class Plant extends JPanel {
-    public int locationX;
-    public int locationY;
-    public int grassRatio = 1;
-
-
-    public void paint(Graphics g) {
-        Image plant = Toolkit.getDefaultToolkit().getImage("plant.png");
-        g.drawImage(plant, 210,210, 100, 100,this);
-    }
+public class Plant extends Animal {
+   
 
 }

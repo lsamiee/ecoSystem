aaea28d82732d6fffbbc741463ddc17075c329c3
Leyla Samiee <lsamie@gmail.com>
2018-05-12 14:34:11 -0400

    Create Animal.java

---
+++ src/Animal.java
@@ -0,0 +1,18 @@
+public class Animal {
+    public int locationX;
+    public int locationY;
+    public int grassRatio = 1;
+
+    private int gainPower () {
+
+    }
+    private int lossPower () {
+
+    }
+    private int moveRandom () {
+
+    }
+    private int anythingElse () {
+
+    }
+}
